3a7e925107d6572a9d630a4204c6ec9ffade4559
Simone Basso <bassosimone@gmail.com>
2018-07-04 13:55:58 +0200

    Improve impl. documentation

--- src/main/java/io/ooni/libndt/api/NDTSettings.java
+++ src/main/java/io/ooni/libndt/api/NDTSettings.java
@@ -8,15 +8,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NDTSettings {
-    // XXX: what about conversion of wider types?
+    // Implementation note: the sizes used by Java are typically larger than
+    // the uses defined inside of libndt.hpp because Java has no unsigned. Yet,
+    // since we always use unsigned, and since Swig performs a type cast to
+    // the original type, narrowing should occur. Thus, we end up with having
+    // only the bits that we care about being set. However, as far as timeouts
+    // are concerned, narrowing would imply that a shorter timeout is set.
+    //
+    // TODO(bassosimone): decide how to deal with this limitation.
+
     public String mlabnsURL = new String();
+
     public String hostname = new String();
+
     public long maxRuntime = 0;
+
     public Map<String, String> metadata = new HashMap<String, String>();
+
     public short nettestFlags = 0;
+
     public String port = new String();
+
     public long protocolFlags = 0;
+
     public String socks5hPort = new String();
+
     public long timeout = 0;
+
     public long verbosity = 0;
 }
