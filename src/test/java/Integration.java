cb587d5b815267273644071e1b3a858883aa72a7
Simone Basso <bassosimone@gmail.com>
2018-07-04 13:59:29 +0200

    Finish the PoC

--- src/test/java/Integration.java
+++ src/test/java/Integration.java
@@ -9,42 +9,59 @@ import io.ooni.libndt.api.NDTConstants;
 import io.ooni.libndt.api.NDTSettings;
 
 class Integration {
-  static class WrapperClient extends NDTClient {
+    static class WrapperClient extends NDTClient {
 
-    WrapperClient() {
-      super();
+        WrapperClient() {
+            super();
+        }
+
+        WrapperClient(NDTSettings s) {
+            super(s);
+        }
+
+        @Override
+        public void onWarning(String s) {
+            System.out.println("Java: WARNING: " + s);
+        }
+
+        @Override
+        public void onInfo(String s) {
+            System.out.println("Java: INFO: " + s);
+        }
+
+        @Override
+        public void onDebug(String s) {
+            System.out.println("Java: DEBUG: " + s);
+        }
+
+        @Override
+        public void onPerformance(short tid, short nflows, double measuredBytes,
+                double measurementInterval, double elapsed,
+                double maxRuntime) {
+            System.out.println("Java: PERFORMANCE");
+        }
+
+        @Override
+        public void onResult(String scope, String name, String value) {
+            System.out.println("Java: RESULT: " + scope + "." + name + " = " + value);
+        }
+
+        @Override
+        public void onServerBusy(String msg) {
+            System.out.println("Java: SERVER_BUSY: " + msg);
+        }
     }
 
-    WrapperClient(NDTSettings s) {
-      super(s);
+    public static void main(String[] args) {
+        // TODO(bassosimone): here we should probably ignore SIGPIPE!
+        System.loadLibrary("ndt-java");
+        NDTSettings settings = new NDTSettings();
+        settings.verbosity = NDTConstants.verbosityInfo;
+        settings.nettestFlags = (short)(NDTConstants.nettestFlagDownload | NDTConstants.nettestFlagUpload);
+        settings.protocolFlags = NDTConstants.protocolFlagJSON;
+        settings.hostname = "ndt.iupui.mlab2.trn01.measurement-lab.org";
+        settings.metadata.put("client.application", "measurement-kit/libndt-java");
+        NDTClient client = new WrapperClient(settings);
+        client.run();
     }
-
-    @Override
-    public void onWarning(String s) {
-      System.out.println("Java: WARNING: " + s);
-    }
-
-    @Override
-    public void onInfo(String s) {
-      System.out.println("Java: INFO: " + s);
-    }
-
-    @Override
-    public void onDebug(String s) {
-      System.out.println("Java: DEBUG: " + s);
-    }
-  }
-
-  public static void main(String[] args) {
-    // TODO(bassosimone): here we should probably ignore SIGPIPE!
-    System.loadLibrary("ndt-java");
-    NDTSettings settings = new NDTSettings();
-    settings.verbosity = NDTConstants.verbosityInfo;
-    settings.nettestFlags = (short)(NDTConstants.nettestFlagDownload | NDTConstants.nettestFlagUpload);
-    settings.protocolFlags = NDTConstants.protocolFlagJSON;
-    settings.hostname = "ndt.iupui.mlab2.trn01.measurement-lab.org";
-    settings.metadata.put("client.application", "measurement-kit/libndt-java");
-    NDTClient client = new WrapperClient(settings);
-    client.run();
-  }
 }
